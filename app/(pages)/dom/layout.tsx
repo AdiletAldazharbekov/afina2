2ce9e4471f9fa47d13ec430edc6e7d29a156aa18
AdiletAldazharbekov <aldajarbekov@gmail.com>
2024-09-16 20:54:40 +0600

    Адаптация меню под мобилку

--- app/(pages)/dom/layout.tsx
+++ app/(pages)/dom/layout.tsx
@@ -15,12 +15,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 		// </div>
 		<div className='flex flex-col h-[100dvh] md:fixed md:top-0 md:bottom-0 md:left-0 md:right-0'>
 			<div className='bg-sky-900 text-white px-4 leading-[4rem]'>Header</div>
-			<div className='flex flex-col md:flex-row grow'>
+			<div className='flex flex-col-reverse md:flex-row grow'>
 				<div className='w-full flex-none bg-sky-900 text-white md:w-64 md:rounded-ee-[2.5rem]'>
 					<Sidebar />
 				</div>
 				<div className='flex flex-col grow bg-sky-900 px-1 md:pl-0'>
-					<div className='flex flex-col h-full rounded-t-2xl bg-white text-sky-900 md:max-h-[100%] md:rounded-ee-[2.5rem] p-2 pb-4'>
+					<div className='flex flex-col h-full rounded-2xl md:rounded-t-2xl bg-white text-sky-900 md:max-h-[100%] md:rounded-ee-[2.5rem] p-2 md:pb-4'>
 						{children}
 					</div>
 				</div>
--- app/(pages)/dom/ui/Sidbar.tsx
+++ app/(pages)/dom/ui/Sidbar.tsx
@@ -6,7 +6,7 @@ import { PowerIcon } from '@heroicons/react/24/outline'
 
 export default function Sidebar() {
 	return (
-		<div className='flex h-full flex-col px-3 py-0 pb-4 md:px-1'>
+		<div className='flex h-full md:flex-col px-3 py-1 md:py-0 md:pb-4 md:px-1'>
 			{/* <div className='my-1 flex h-20 w-full items-center justify-center rounded-md bg-sky-900 text-white md:h-20'>
 				Logo
 			</div> */}
@@ -25,10 +25,10 @@ export default function Sidebar() {
 				>
 					<a
 						href='/'
-						className='flex h-[48px] grow items-center justify-center gap-3 rounded-md bg-sky-900 p-3 text-sm font-medium hover:text-sky-400 md:flex-none md:justify-start md:p-2 md:px-3'
+						className='flex h-[48px] grow items-center justify-center gap-3 rounded-md rounded-ee-2xl bg-sky-900 p-3 text-sm font-medium hover:text-sky-400 md:flex-none md:justify-start md:p-2 md:px-3'
 					>
 						<PowerIcon className='w-6' />
-						<p className='hidden md:block'>Exit</p>
+						<p className='hidden md:block'>Выйти</p>
 					</a>
 
 					{/* <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-inherit p-3 text-sm font-medium hover:text-blue-300 md:flex-none md:justify-start md:p-2 md:px-3'>
