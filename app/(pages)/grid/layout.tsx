45db0bca4db73205fb4457bf4a6df99650d168fe
AdiletAldazharbekov <aldajarbekov@gmail.com>
2024-09-17 00:19:44 +0600

    Fix sidebar

--- app/(pages)/grid/layout.tsx
+++ app/(pages)/grid/layout.tsx
@@ -1,12 +1,10 @@
-import Sidebar from '../dom/ui/Sidbar'
-
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className='flex flex-col h-[100dvh] md:fixed md:top-0 md:bottom-0 md:left-0 md:right-0'>
 			<div className='bg-sky-900 text-white px-4 leading-[4rem]'>Header</div>
 			<div className='flex flex-col-reverse md:flex-row grow'>
 				<div className='w-full flex-none bg-sky-900 text-white md:w-64 md:rounded-ee-[2.5rem]'>
-					<Sidebar />
+					Test{' '}
 				</div>
 				<div className='flex flex-col grow bg-sky-900 px-1 md:pl-0'>
 					<div className='flex flex-col h-full rounded-2xl md:rounded-t-2xl bg-white text-sky-900 md:max-h-[100%] md:rounded-es-none md:rounded-ee-[2.5rem] p-2 md:pb-4'>
