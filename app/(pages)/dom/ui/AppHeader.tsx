05722edd9d65a0a4b3c79781ea94ea910114d2b3
AdiletAldazharbekov <aldajarbekov@gmail.com>
2024-09-20 00:06:50 +0600

    fix headerss

--- app/(pages)/dom/ui/AppHeader.tsx
+++ app/(pages)/dom/ui/AppHeader.tsx
@@ -31,7 +31,15 @@ const AppHeader: React.FC = () => {
 		<header className='bg-sky-800 p-4'>
 			<div className='container mx-auto flex justify-between items-center'>
 				<div className='text-white text-xl font-bold'>
-					<a href='#'>MyLogo</a>
+					<a href='#'>
+						<Image
+							// src='/logo.png'
+							src='https://nextjs.org/icons/next.svg'
+							alt='Logo'
+							width={100}
+							height={100}
+						/>
+					</a>
 				</div>
 
 				<div className='relative' ref={dropdownRef}>
--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,8 @@ export default function Home() {
 				<main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
 					<Image
 						className='dark:invert'
-						src='https://nextjs.org/icons/next.svg'
+						src='/Logo2x.jpg'
+						// src='https://nextjs.org/icons/next.svg'
 						alt='Next.js logo'
 						width={180}
 						height={38}
